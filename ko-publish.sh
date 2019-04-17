2272bbd796de47fdf3a0c0b0472d7753773e13c2
Justin Brulotte <justin.brlotte797@gmail.com>
2019-04-17 13:25:12 -0400

    Try path on crane

--- ko-publish.sh
+++ ko-publish.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+crane pull docker.appdirect.tools/appdirect-hello-world-function/hello-world-golang-function:latest out.tar && crane push out.tar docker.appdirect.tools/appdirect-hello-world-function/hello-world-golang-function:latest
+
 ko publish github.com/knative/serving/cmd/controller
 docker images ls
