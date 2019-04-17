b1541821d67dfe7a30487e0d4289a60092ca5921
Justin Brulotte <justin.brlotte797@gmail.com>
2019-04-17 11:42:40 -0400

    crane push

--- ko-publish.sh
+++ ko-publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 crane image docker.appdirect.tools/appdirect-hello-world-function/hello-world-nodejs-function:latest
 
+crane pull docker.appdirect.tools/appdirect-hello-world-function/hello-world-nodejs-function:latest out.tar && crane push out.tar docker.appdirect.tools/appdirect-hello-world-function/hello-world-nodejs-function:latest
+
 ko publish github.com/knative/serving/cmd/controller
