8d4635e53d295a7a0173449045bde8f07833363b
Justin Brulotte <justin.brlotte797@gmail.com>
2019-04-18 12:47:53 -0400

    Change push

--- ko-publish.sh
+++ ko-publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ko publish github.com/knative/serving/cmd/controller
 
-docker pull docker.appdirect.tools/appdirect-hello-world-function/hello-world-golang-function:latest
-docker push appdirect-hello-world-function/hello-world-golang-function:latest
+docker pull docker.appdirect.tools/appdirect-hello-world-function/hello-world-golang-function
+docker push docker.appdirect.tools/appdirect-hello-world-function/hello-world-golang-function
 
 docker images ls
